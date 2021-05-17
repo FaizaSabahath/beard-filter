fa595bd4f4d060a64fe51aa3cdd333e23b7df2fa
FaizaSabahath <76249127+FaizaSabahath@users.noreply.github.com>
2021-05-17 17:20:44 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){}
+function setup(){
+    canvas = createCanvas(300,300);
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    canvas.position(550,200)
+}
+function draw(){
+    
+}
